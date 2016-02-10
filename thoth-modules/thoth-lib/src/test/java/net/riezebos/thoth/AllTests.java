34e6305758f13c4bead024bf0d2c4434dd5834f3
Wido Riezebos <w.riezebos@eyefreight.com>
2016-02-10 23:08:48 +0100

    More tests

---
+++ thoth-modules/thoth-lib/src/test/java/net/riezebos/thoth/AllTests.java
@@ -0,0 +1,33 @@
+package net.riezebos.thoth;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import net.riezebos.thoth.beans.BookClassificationTest;
+import net.riezebos.thoth.beans.BookTest;
+import net.riezebos.thoth.beans.BookmarkTest;
+import net.riezebos.thoth.beans.BookmarkUsageTest;
+import net.riezebos.thoth.beans.ContentNodeTest;
+import net.riezebos.thoth.beans.MarkDownDocumentTest;
+import net.riezebos.thoth.markdown.FileProcessorTest;
+import net.riezebos.thoth.markdown.critics.CommentTranslatorTest;
+import net.riezebos.thoth.markdown.critics.CriticMarkupProcessorTest;
+import net.riezebos.thoth.markdown.critics.DeleteTranslatorTest;
+import net.riezebos.thoth.markdown.critics.HighlightTranslatorTest;
+import net.riezebos.thoth.markdown.critics.InsertTranslatorTest;
+import net.riezebos.thoth.markdown.critics.SubstitutionTranslatorTest;
+import net.riezebos.thoth.markdown.util.DocumentNodeTest;
+import net.riezebos.thoth.markdown.util.LineInfoTest;
+import net.riezebos.thoth.markdown.util.ProcessorErrorTest;
+import net.riezebos.thoth.markdown.util.SoftLinkTranslationTest;
+import net.riezebos.thoth.util.ThothUtilTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({BookClassificationTest.class, BookmarkTest.class, BookmarkUsageTest.class, BookTest.class, ContentNodeTest.class, CommentTranslatorTest.class,
+    CriticMarkupProcessorTest.class, DeleteTranslatorTest.class, HighlightTranslatorTest.class, InsertTranslatorTest.class, SubstitutionTranslatorTest.class,
+    DocumentNodeTest.class, LineInfoTest.class, ProcessorErrorTest.class, SoftLinkTranslationTest.class, MarkDownDocumentTest.class, FileProcessorTest.class,
+    ThothUtilTest.class})
+public class AllTests {
+
+}
--- thoth-modules/thoth-lib/src/test/java/net/riezebos/thoth/markdown/critics/InsertTranslatorTest.java
+++ thoth-modules/thoth-lib/src/test/java/net/riezebos/thoth/markdown/critics/InsertTranslatorTest.java
@@ -1,6 +1,7 @@
 package net.riezebos.thoth.markdown.critics;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,6 +24,25 @@ public class InsertTranslatorTest {
     assertTrue(matcher.find(0));
     translated = insertTranslator.translate(matcher);
     assertEquals("<ins> insert </ins>", translated);
+
+    insertTranslator = new InsertTranslator(CriticProcessingMode.TRANSLATE_ONLY);
+    matcher = Pattern.compile(CriticMarkupProcessor.INSERTION_PATTERN, Pattern.DOTALL).matcher("{++\n\n insert ++}");
+    assertTrue(matcher.find(0));
+    translated = insertTranslator.translate(matcher);
+    assertEquals("<ins>   insert </ins>", translated);
+
+    insertTranslator = new InsertTranslator(CriticProcessingMode.TRANSLATE_ONLY);
+    matcher = Pattern.compile(CriticMarkupProcessor.INSERTION_PATTERN, Pattern.DOTALL).matcher("{++\n\n++}");
+    assertTrue(matcher.find(0));
+    translated = insertTranslator.translate(matcher);
+    assertEquals("<ins>  </ins>", translated);
+
+    insertTranslator = new InsertTranslator(CriticProcessingMode.TRANSLATE_ONLY);
+    matcher = Pattern.compile(CriticMarkupProcessor.INSERTION_PATTERN, Pattern.DOTALL).matcher("{++something\n\n++}");
+    assertTrue(matcher.find(0));
+    translated = insertTranslator.translate(matcher);
+    assertEquals("<ins>something  </ins>\n\n<ins class=\"critic break\">&nbsp;</ins>\n\n", translated);
+
   }
 
 }
